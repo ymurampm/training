b366b45e1f690ab50642913539c86438456e6f52
ymura <ymuragg@gmail.com>
2021-07-13 08:57:47 +0900

    add new JS file

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// Write a Javascript code to input time in minutes and convert it hours and minutes. Eg: 400 minutes =6 hours and 40 minutes
+
+// var wholeMinutes=parseInt(prompt("Input minutes"));
+// var hours=parseInt(wholeMinutes / 60)
+// var minutes=wholeMinutes % 60
+// var result = hours +" hours " + minutes +" minutes"
+// alert(result)
+
+//Write a Javascript code to input two numbers and swap their values
+
+var num1 = prompt("Input first number"); 
+var num2 = prompt("Input second number"); 
+var tempnum = num1;
+num1 = num2;
+num2 = tempnum;
+alert("num1 =" + num1 + "  num2 =" + num2)
